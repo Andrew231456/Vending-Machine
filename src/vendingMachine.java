00829d0cb50c60b2b36d4f7e2ce94200002d0b81
Andrew231456 <andrewmatos203@gmail.com>
2021-05-09 18:04:04 -0400

    Removed useless lines in the code

--- src/vendingMachine.java
+++ src/vendingMachine.java
@@ -5,35 +5,26 @@
 */
 import java.util.Scanner;
 
-public class vendingMachine
-{
-    public static void main(String[] args)
-    {
+public class vendingMachine {
+    public static void main(String[] args) {
     	// Variables
     	Double A1,C7,A2,B4,B5,G8,A9;// Vending codes
     	A1 = C7 = 1.50; //$1 for code
     	A2 = B4 = B5 = G8 = 2.00; //2$ Amount for code
     	A9 = 3.00; 	// 3$ for code
-    	
     	// Prints
         System.out.println("Welcome to the best snack machine of your life! This machine only accepts $1 bills. Please insert money.");
         System.out.println("Vending Codes:\n[A1] Snickers - $1.50, [A2] Chips - $2.00,  [B4] Gatorade - $2.00,  [C7] Reese's - $1.50, [A9] Trail Mix - $3.00, \n [B5] Hershey's - $2.00, [G8] Granola Bar - $2.00\nHow many $1 bills would you like to enter?");
-       
         Scanner input = new Scanner(System.in);
         Double amount = input.nextDouble();
         int amountInt = (int)Math.round(amount); 
-        System.out.println("You have put in " + amountInt + " dollar bills\nPlease enter your selected snack code number (Ex. A1,A2,B1,...).");
-       
+        System.out.println("You have put in " + amountInt + " dollar bills\nPlease enter your selected snack code number (Ex. A1,A2,B1,...).");   
         Scanner input2 = new Scanner(System.in);
-        // SELECT YOUR SNACK
-        String snack = input2.nextLine();
-       
+        String snack = input2.nextLine();// SELECT YOUR SNACK
         System.out.println("You have selected " + snack + ". One moment please.");
         System.out.println("Are you happy with your selection? If not cancel now by entering 'no'. If you are happy enter 'yes'.");
-      
         String decision = input2.nextLine();
-        // if decision is no
-        if(decision.equalsIgnoreCase("no")) {
+        if(decision.equalsIgnoreCase("no")) {// if decision is no
             Double amountLeft = 0.0;
             System.out.println("We're sorry you are not happy with your choice. Here is a full refund.");
             System.out.format("Amount Left: $%.2f", amountLeft);
@@ -55,9 +46,8 @@ public class vendingMachine
                  String snack1 = input5.nextLine();
                 
                  System.out.println("You have selected " + snack + ". One moment please.");
-             }
-            // if decision is yes
-        } else if (decision.equalsIgnoreCase("yes")) {
+             }  
+        } else if (decision.equalsIgnoreCase("yes")) { // if decision is yes
             Double amountLeft = 0.0;
             String snackName = "";
             // Switches for snacks
@@ -106,11 +96,9 @@ public class vendingMachine
             System.out.println("\nReturning change...");
             System.out.format("\nAmount Returned: $%.2f", amountLeft);
             System.out.println("\n You chose "+ snackName +"! What a great choice! That is one of my favorites, I hope you enjoy it too! \nHope to see you again real soon!");
-        }
-        else {
+        } else {
             System.out.println("Invalid feedback. Cancelling Transaction.");
             System.out.format("\nAmount Returned: $%.2f", amount);
         }
-        
     }
 }
